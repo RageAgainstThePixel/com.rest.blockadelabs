48f48c05da19947ae72e173e127bd40e5c7745e1
Stephen Hodgson <hodgson.designs@gmail.com>
2023-05-21 20:58:22 -0400

    Initial Project setup

--- BlockadeLabs/Packages/com.rest.blockadelabs/Tests/ExampleTestScript.cs
+++ BlockadeLabs/Packages/com.rest.blockadelabs/Tests/ExampleTestScript.cs
@@ -1,8 +1,8 @@
-﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using NUnit.Framework;
 
-namespace ProjectScope.ProjectName.Tests
+namespace Rest.BlockadeLabs.Tests
 {
     internal class ExampleTestScript
     {
